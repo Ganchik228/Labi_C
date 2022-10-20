6aa696665655ac8b33485c87568f3007634cf744
zevoK <RedosGaros@yandex.ru>
2022-10-20 13:04:09 +0500

    token add

--- laba3.c
+++ laba3.c
@@ -2,15 +2,18 @@
 #include <math.h>
 int main()
 {
-    double x;
+    double x,f;
     int i=1;
     printf("\tx\t f(x)\n");
     printf("---------------------------\n");
+    scanf("%lf",&x);
     while(i<=10)
     {
-        if (x=>0 && x<=1)
+        if (x>=0 && x<=1)
         {
-            
+            f=(cos(x))*pow(M_E,pow(-x,2));
+            printf("%lf",f);
+            i++;
         }
     }
     return 0;
