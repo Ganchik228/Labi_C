8cb428f6c97a8e828d7e332646ab9764a0daa1eb
Ganchik228 <ganchikgamer@gmail.com>
2022-12-22 05:58:47 +0500

    gotova tolki 6

--- laba9.c
+++ laba9.c
@@ -1,15 +1,51 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 #include <stdlib.h>
-int main()
-{
-  char str[256];
-  char del[]=", ";
-  char *p = strtok(str, delim);
-  printf("Please enter a string : ");
-  scanf("%[^\n]",str);
-  int len= strlen(str),a[len];
-  printf("String =%s",str);
-  return 0;
+#include <ctype.h>
+#include <string.h>
+int is_space(char c) {
+    return c == ' ';
 }
+int is_not_space(char c) {
+    return 0 == is_space(c);
+}
+int count_start_if(char* str, int (*pred)(char)) {
+    int count = 0;
+    
+    while (*str != 0) {
+        if (pred(*str)) {
+            count++;
+            str++;
+        }
+        else
+            break;
+    }
+    return count;
+}
+int count_words(char* str) {
+    int length = strlen(str);
+    int count = 0;
+    int i = 0;
+    while (1) {
+        int spaces_count = count_start_if(&str[i], is_space);
+        i += spaces_count;
+        if (i >= length) {
+            break;
+        }   
+        int word_length = count_start_if(&str[i], is_not_space);
+        if (word_length > 0) {
+            count++;
+        }  
+        i += word_length;
+        if (i >= length) {
+            break;
+        }
+    }
+    return count;
+}
+int main() {
+    char str[255];
+    printf("enter string: ");
+    scanf("%255[^\n]", str);
+    printf("words count: %d", count_words(str));
+    return 0;
+}  
